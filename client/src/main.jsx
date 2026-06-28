f278709e4ef6194a463cd80237752b1877bc1f01
Sarah Thomson <sthom120@gmail.com>
2026-06-28 17:10:08 +1000

    Add shared footer with AI disclaimer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,35 +8,56 @@ import LoginPage from './pages/LoginPage.jsx';
 import TourismOperatorDashboard from './pages/TourismOperatorDashboard.jsx';
 import GovernmentDashboard from './pages/GovernmentDashboard.jsx';
 import AdminUsersPage from './pages/AdminUsersPage.jsx';
+import AppFooter from './components/AppFooter.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={
-            <ProtectedRoute>
-              <App />
-            </ProtectedRoute>
-          } />
-          <Route path="/operator" element={
-            <ProtectedRoute>
-              <TourismOperatorDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/government" element={
-            <ProtectedRoute>
-              <GovernmentDashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/admin/users" element={
-            <ProtectedRoute>
-              <AdminUsersPage />
-            </ProtectedRoute>
-          } />
-        </Routes>
+        <div className="app-frame">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <App />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/operator"
+              element={
+                <ProtectedRoute>
+                  <TourismOperatorDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/government"
+              element={
+                <ProtectedRoute>
+                  <GovernmentDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <AdminUsersPage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+
+          <AppFooter />
+        </div>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
